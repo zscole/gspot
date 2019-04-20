64dea8eb4f3c92624deb1016e309fa59475911f8
Pedro Gomes <pedrogomes94@gmail.com>
2019-04-21 00:20:36 +0200

    Update content.js

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/g(o|0)v(e|3)rn(a|4)nc(e|3)/gi, 'assplay');
+            var replacedText = text.replace(/g(o|0)v(e|3)rn(a|4)nc(e|3)/gi, 'assplay').replace(/(a|4)(s|5)(s|5)p(l|1)(a|4)y/gi, 'governance');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
